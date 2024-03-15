eb5c20ac467ce63cd1d7d825c72d42c17d8f5e81
radian628 <70352880+radian628@users.noreply.github.com>
2024-03-14 20:14:44 -0700

    got firebase working!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation ("androidx.fragment:fragment:1.4.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
