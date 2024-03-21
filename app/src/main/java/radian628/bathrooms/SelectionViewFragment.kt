8113ffed359a59239fc52f9e130869fc8f18370f
doehrins <doehrins@oregonstate.edu>
2024-03-20 21:28:42 -0700

    selection view issue fixed

--- app/src/main/java/radian628/bathrooms/SelectionViewFragment.kt
+++ app/src/main/java/radian628/bathrooms/SelectionViewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -16,12 +17,14 @@ import com.google.firebase.firestore.toObjects
 class SelectionViewFragment: Fragment(R.layout.selection_view_main) {
     private val viewModel: SelectionViewViewModel by viewModels()
     private lateinit var floorRecyclerView: RecyclerView
+    private lateinit var buildingTitle: TextView
 //    private val args:
 //    private lateinit var floorList : ArrayList<Floor>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val closeBtn = view.findViewById<ImageView>(R.id.arrow_right)
+        buildingTitle = view.findViewById<TextView>(R.id.building_name)
         floorRecyclerView = view.findViewById(R.id.main_recyclerview)
         floorRecyclerView.setHasFixedSize(true)
         floorRecyclerView.layoutManager = LinearLayoutManager(view.context)
@@ -38,10 +41,19 @@ class SelectionViewFragment: Fragment(R.layout.selection_view_main) {
 
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
         val buildingName = arguments?.getString("building_name") ?: ""
+        buildingTitle.text = buildingName
         viewModel.loadData(buildingName)
+
     }
+
+//    override fun onStart() {
+//        super.onStart()
+//        val buildingName = arguments?.getString("building_name") ?: ""
+//        Log.d("BUILDING NAME", buildingName)
+//        viewModel.loadData(buildingName)
+//    }
 
 }
